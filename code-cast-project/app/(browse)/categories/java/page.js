ad3eb033c83f9698cd8985bba78937d9edca597a
edelt1125 <eric.deltoro@outlook.com>
2024-05-02 15:42:28 -0500

    added missing module

--- code-cast-project/app/(browse)/categories/java/page.js
+++ code-cast-project/app/(browse)/categories/java/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Card, CardHeader, Avatar, Button } from "nextui-org/react";
+import { Card, CardHeader, Avatar, Button } from "@nextui-org/react";
 import { motion } from "framer-motion";
 
 const breatheAnimation = {
