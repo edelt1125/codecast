83da8bd49885e4bf612d7cf89e84b973400fca46
edelt1125 <eric.deltoro@outlook.com>
2024-05-13 16:30:46 -0500

    Fix Cache Issues

--- code-cast-project/app/api/commentsAPI/route.js
+++ code-cast-project/app/api/commentsAPI/route.js
@@ -1,21 +1,31 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 const { db } = require("@/lib/db");
 
-export async function GET(req) {
-    const comments = await db.comment.findMany({
-    include: {
-      user: true,
-    },
-    orderBy: {
-      createdAt: 'desc'
+export async function POST(req) {
+    try {
+        const data = await req.json();
+        const newComment = await db.comment.create({
+            data: {
+                ...data,
+                createdAt: new Date(),
+            },
+        });
+
+        // Revalidate the comments page
+        await revalidatePath("/community");
+        console.log("Revalidated path");
+
+        return new Response(JSON.stringify(newComment), {
+            status: 201,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+            },
+        });
+    } catch (error) {
+        console.error("Error in POST /api/comments:", error);
+        return new Response("Internal Server Error", { status: 500 });
     }
-  });
-  return new Response(JSON.stringify(comments), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-    },
-  });
 }
